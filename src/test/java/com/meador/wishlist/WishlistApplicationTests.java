aa7bf9bdd12d9c421d81e2605a32f9817b0e6c67
Tyler Meador <tmeador13@gmail.com>
2021-11-09 11:11:59 -0600

    Initial Commit

---
+++ src/test/java/com/meador/wishlist/WishlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meador.wishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
